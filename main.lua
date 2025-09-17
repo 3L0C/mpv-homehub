dcd64421d7463d09a3b30da2c1eda7245ded49fc
user <141392286+3L0C@users.noreply.github.com>
2025-09-17 16:57:26 -0700

    fix: simplify main

--- main.lua
+++ main.lua
@@ -1,15 +1,8 @@
 --[[
-    mpv-homehub
-
-    This script turns mpv into a minimal client for your favorite home media server(s).
+--  mpv-homehub
+--
+--  This script turns mpv into a minimal client for your favorite home media server(s).
 --]]
-
-local mp = require 'mp'
-
-local o = require 'src.core.options'
-
--- setting the package paths
-package.path = mp.command_native({'expand-path', o.module_directory}) .. '/?.lua;' .. package.path
 
 local homehub = require 'src.core.init'
 homehub.start()
