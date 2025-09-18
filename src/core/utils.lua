5fc56caf06bc05c1627bb9adb861ab1437b9a16f
user <141392286+3L0C@users.noreply.github.com>
2025-09-17 17:12:21 -0700

    add(utils): provide handler template

--- src/core/utils.lua
+++ src/core/utils.lua
@@ -1,10 +1,37 @@
 --[[
-    Utility functions
+--  Utility functions
 --]]
 
----@class hh_utils
-local hh_utils = {
+local utils = require 'mp.utils'
 
-}
+local events = require 'src.core.events'
+
+---@alias HandlerTable table<EventName,ListenerCB>
+
+---@class hh_utils
+local hh_utils = {}
+
+---Handler template for various controllers.
+---@param event_name EventName
+---@param data EventData
+---@param handlers HandlerTable
+---@param component ComponentName
+function hh_utils.handler_template(event_name, data, handlers, component)
+    local fn = handlers[event_name]
+    if type(fn) ~= 'function' then
+        events.emit('msg.warn.' .. component, { msg = {
+            'Got unhandled event:', event_name
+        } })
+    else
+        local success, err = pcall(fn, event_name, data)
+        if not success then
+            events.emit('msg.error.' .. component, {msg = err})
+        else
+            events.emit('msg.debug.' .. component, { msg = {
+                ("Handled event '%s' with data '%s'."):format(event_name, utils.to_string(data))
+            } })
+        end
+    end
+end
 
 return hh_utils
